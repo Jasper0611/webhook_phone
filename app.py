a13a8bb37aed11d1676bf9ffbd20225208a88e40
Jasper0611 <rahu.jasper7@gmail.com>
2024-02-11 20:39:09 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
 import requests
+import logging
 
 app = Flask(__name__)
-
+logging.basicConfig(filename='webhook.log', level=logging.INFO)
 @app.route("/webhook", methods=['POST'])
 def app_():
     data = request.get_json()
-    print(data)
+    logging.info(f'Received data: {data}')
     return 'Success'
