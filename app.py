53116c2075f4004964c4a8df80a8a86bc16a1216
Jasper0611 <rahu.jasper7@gmail.com>
2024-02-12 21:05:48 +0530

    first commit

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ app = Flask(__name__)
 @app.route("/webhook", methods=['POST'])
 def app_():
     if request.method == "POST":
-        print(request.json())
+        print(request.json)
         return "success"
     else:
         abort(400)
-
-if __name__ == "__main__":
-    app.run()
