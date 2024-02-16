774e7e6ab3176530113271799a13b2efb4a13192
Jasper0611 <rahu.jasper7@gmail.com>
2024-02-16 11:04:13 +0530

    First commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, abort
-from flask_mysql_connector import MySQL
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] ='localhost'
